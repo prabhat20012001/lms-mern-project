5486b5e668f3135ab17e759bfd54f5330828d7c5
prabhat20012001 <https://github.com/prabhat20012001>
2024-03-30 12:31:28 +0530

    user schema design done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,5 +2,10 @@ import { Router } from "express";
 
 const router=Router()
 
+router.post('/register',register)
+router.post('/login',login)
+router.get('/logout',logout)
+router.get('/me',getProfile)
+
 
 export default router;
